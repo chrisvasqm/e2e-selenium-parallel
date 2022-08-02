494460a302deab0993a7852274281bc3f7393306
chrisvasqm <christianvasquezmelo@gmail.com>
2022-08-02 19:14:32 -0400

    Add print statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        var driver = new Driver();
-        var login = new LoginPage(driver);
-
-        var home = login.signIn("asd", "dsa");
-        home.isLoggedIn();
+        System.out.println("Hello world");
     }
 
 }
